c86293cc2ca04b8163512f69dc32478d99d8d230
bacnguyencong <nguyencongbacbk@gmail.com>
2026-04-22 08:45:43 +0000

    Deploying to gh-pages from @ bacnguyencong/bacnguyencong.github.io@1126d7bb1a7542807052bad1586e8ed34d38cbc6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/talks/";
           },
-        },{id: "post-drifting-models",
+        },{id: "post-conditional-flow-matching",
+        
+          title: "Conditional flow matching",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/conditional-flow-matching/";
+          
+        },
+      },{id: "post-drifting-models",
         
           title: "Drifting models",
         
